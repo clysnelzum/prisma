33859c7c39fb6a81dc10d82b2f79fa99de283c34
CLZ <clysnelzum@gmail.com>
2026-09-23 11:29:06 -0300

    fix(ci): resolve packaging issues across Windows, Linux, and macOS release jobs

--- cmake/vcpkg-triplets/universal-osx.cmake
+++ cmake/vcpkg-triplets/universal-osx.cmake
@@ -1,6 +1,6 @@
 # See https://github.com/microsoft/vcpkg/discussions/19454
 # NOTE: Try to keep in sync with default arm64-osx definition
-set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
